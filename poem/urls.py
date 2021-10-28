73fdae3e8acdef66db5d7bc9849defea8785d165
Mumtoz Valijonov <mumtozvalijonov@gmail.com>
2021-10-28 18:59:39 +0500

    Use class based views

--- poem/urls.py
+++ poem/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_all_poems, name='poems'),
-    path('<int:poem_id>', views.get_poem_by_id, name='retrieve_poem'),
+    path('', views.PoemsView.as_view(), name='poems'),
+    path('<int:pk>', views.PoemDetailView.as_view(), name='retrieve_poem'),
     path('<int:poem_id>/update', views.update_poem_by_id, name='update_poem'),
     path('<int:poem_id>/like', views.like_poem),
     path('<int:poem_id>/dislike', views.dislike_poem),
--- poem/views.py
+++ poem/views.py
@@ -3,47 +3,83 @@ from django.shortcuts import redirect, render, reverse
 from django.contrib.auth.decorators import login_required
 from django.db.utils import IntegrityError
 from django.db.models import Q
+from django.views.generic import ListView, DetailView
 
 from poem.forms import CommentForm, PoemForm
 
 from .models import Poem
 
 
-@login_required(login_url='/accounts/login')
-def get_all_poems(request):
-    search = request.GET.get('search', '')
-    sort_by_author = request.GET.get('sort_by_author', 'asc')
-    sort_by_name = request.GET.get('sort_by_name', 'asc')
-    sorting = (
-        'author__last_name' if sort_by_author == 'asc' else '-author__last_name',
-        'name' if sort_by_name == 'asc' else '-name'
-    )
-    search_filter = []
-    if search:
-        search_filter.append(
-            Q(name__icontains=search) |
-            Q(author__first_name__icontains=search) |
-            Q(author__last_name__icontains=search)
+class PoemsView(ListView):
+    model = Poem
+    paginate_by = 1
+
+    def get_queryset(self):
+        search = self.request.GET.get('search', '')
+        search_filter = []
+        if search:
+            search_filter.append(
+                Q(name__icontains=search) |
+                Q(author__first_name__icontains=search) |
+                Q(author__last_name__icontains=search)
+            )
+
+        ordering = self.get_ordering()
+        return Poem.objects.filter(*search_filter).order_by(*ordering)
+
+    def get_ordering(self):
+        sort_by_author = self.request.GET.get('sort_by_author', 'asc')
+        sort_by_name = self.request.GET.get('sort_by_name', 'asc')
+        sorting = (
+            'author__last_name' if sort_by_author == 'asc' else '-author__last_name',
+            'name' if sort_by_name == 'asc' else '-name'
         )
-    poems = Poem.objects.filter(*search_filter).order_by(*sorting)
-    return render(
-        request,
-        'poem/poems.html',
-        context={
-            'poems': poems,
-            'search': search,
-            'sort_by_author': sort_by_author,
-            'sort_by_name': sort_by_name
-        }
-    )
+        return sorting
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(*kwargs)
+        context['search'] = self.request.GET.get('search', '')
+        context['sort_by_author'] = self.request.GET.get('sort_by_author', '')
+        context['sort_by_name'] = self.request.GET.get('sort_by_name', '')
+        return 
 
 
-@login_required(login_url='/accounts/login')
-def get_poem_by_id(request, poem_id):
-    poem = Poem.objects.filter(id=poem_id).first()
-    if poem:
-        return render(request, 'poem/poem.html', context={'poem': poem, 'comment_form': CommentForm()})
-    return render(request, '404.html')
+# @login_required(login_url='/accounts/login')
+# def get_all_poems(request):
+#     search = request.GET.get('search', '')
+#     sort_by_author = request.GET.get('sort_by_author', 'asc')
+#     sort_by_name = request.GET.get('sort_by_name', 'asc')
+#     sorting = (
+#         'author__last_name' if sort_by_author == 'asc' else '-author__last_name',
+#         'name' if sort_by_name == 'asc' else '-name'
+#     )
+#     search_filter = []
+#     if search:
+#         search_filter.append(
+#             Q(name__icontains=search) |
+#             Q(author__first_name__icontains=search) |
+#             Q(author__last_name__icontains=search)
+#         )
+#     poems = Poem.objects.filter(*search_filter).order_by(*sorting)
+#     return render(
+#         request,
+#         'poem/poems.html',
+#         context={
+#             'poems': poems,
+#             'search': search,
+#             'sort_by_author': sort_by_author,
+#             'sort_by_name': sort_by_name
+#         }
+#     )
+
+
+class PoemDetailView(DetailView):
+    model = Poem
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['comment_form'] = CommentForm()
+        return context
 
 
 @login_required(login_url='/accounts/login')
